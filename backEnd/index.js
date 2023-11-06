09936a195c4eed8da7f6c18809cef9accf2aeecd
tamim-18 <tamim11903060@gmail.com>
2023-11-06 12:00:44 +0600

    new version added

--- backEnd/index.js
+++ backEnd/index.js
@@ -1,7 +1,27 @@
 import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
+import connectDb from "./mongodb/connect.js";
+import postRoutes from "./routes/postRoutes.js";
+import delleRoutes from "./routes/delleRoutes.js";
 dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
+app.use("/api/v1/posts", postRoutes);
+app.use("/api/v1/delle", delleRoutes);
+app.get("/", async (req, res) => {
+  res.status(200).json({
+    message: "Hello from DALL.E!",
+  });
+});
+const startServer = async () => {
+  try {
+    connectDb(process.env.MONGODB_URL);
+    app.listen(8080, () => console.log("Server started on port 8080"));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
